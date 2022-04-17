b9cf4a59f51654c7d084c8fcaf3b35777313b530
BernardSapida <bernardsapida1706@gmail.com>
2022-04-17 18:11:58 +0800

    commit

--- src/views/Multiplayer.vue
+++ src/views/Multiplayer.vue
@@ -1,4 +1,9 @@
 <template>
+    <header class="header-newgame">
+        <nav>
+            <button type="button" @click="goBack"><fa icon="arrow-left"/></button>
+        </nav>
+    </header>
     <div>
         <h1>MULTIPLAYER PAGE</h1>
     </div>
